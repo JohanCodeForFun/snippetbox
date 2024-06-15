2f181953c726e8bb30f3ca7ebdec64160f716b8e
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-06-15 21:17:56 +0200

    session management with scs package and mysqlstore

--- db/migrations/001_create_tables.sql
+++ db/migrations/001_create_tables.sql
@@ -5,3 +5,9 @@ content TEXT NOT NULL,
 created DATETIME NOT NULL,
 expires DATETIME NOT NULL
 );
+
+CREATE TABLE sessions (
+token CHAR(43) PRIMARY KEY, data BLOB NOT NULL,
+expiry TIMESTAMP(6) NOT NULL
+);
+CREATE INDEX sessions_expiry_idx ON sessions (expiry);
